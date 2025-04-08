35699368ec0bec669467c9b3ba85e3d0c695af99
Gustav Pettersson Bjorklund <gustav.pettersson2@outlook.com>
2025-04-08 12:10:21 +0200

    updated telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,20 +6,26 @@ return {
       local builtin  = require("telescope.builtin")
       vim.keymap.set('n', '<C-p>', builtin.find_files, {})
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+      vim.keymap.set('n', '<leader>fr', function()
+        local old_text = vim.fn.input("Replace: ")
+        local new_text = vim.fn.input("With: ")
+        -- This command will run the substitution on each file in the quickfix list.
+        vim.cmd("cfdo %s/" .. old_text .. "/" .. new_text .. "/gc | update")
+      end, { desc = "Replace text in quickfix list" })
     end,
   },
   {
     "nvim-telescope/telescope-ui-select.nvim",
     config = function()
       require("telescope").setup({
-        extentions = {
+        extensions = {
           ["ui-select"] = {
-            require("telescope.themes").get_dropdown {
-            }
-          }
-        }
+            require("telescope.themes").get_dropdown {},
+          },
+        },
       })
       require("telescope").load_extension("ui-select")
     end,
-    },
+  },
 }
+
