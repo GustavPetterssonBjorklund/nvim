680954cedf65bff120d5bb1b187d465cbbf78fcb
Gustav Pettersson <gustav.pettersson2@outlook.com>
2025-11-19 12:15:53 +0100

    might work?

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,42 +1,44 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        opts = {
-            ui = {
-                icons = {
-                    package_installed = "✓",
-                    package_pending = "➜",
-                    package_uninstalled = "✗",
-                },
-            },
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
         },
+      },
     },
+  },
 
-    {
-        "williamboman/mason-lspconfig.nvim",
-        dependencies = {
-            "williamboman/mason.nvim",
-            "neovim/nvim-lspconfig",
-        },
-        opts = {
-            ensure_installed = {
-                "lua_ls",
-                "pyright",
-                "rust_analyzer",
-                "clangd",
-            },
-            automatic_installation = true, -- auto-install missing LSPs
-        },
-        config = function(_, opts)
-            require("mason-lspconfig").setup(opts)
-
-            -- Auto-setup every installed server
-            require("mason-lspconfig").setup_handlers({
-                function(server)
-                    require("lspconfig")[server].setup({})
-                end,
-            })
-        end,
+  {
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = {
+      "mason-org/mason.nvim",
+      "neovim/nvim-lspconfig",
     },
+    opts = {
+      ensure_installed = {
+        "lua_ls",
+        "pyright",
+        "rust_analyzer",
+        "clangd",
+      },
+      -- new API: automatic_enable replaces setup_handlers/automatic_installation
+      automatic_enable = true, -- this is the default, but explicit is nice
+    },
+    config = function(_, opts)
+      require("mason-lspconfig").setup(opts)
+
+      -- LSP settings are now done *outside* mason-lspconfig
+      local lspconfig = require("lspconfig")
+
+      lspconfig.lua_ls.setup({})
+      lspconfig.pyright.setup({})
+      lspconfig.rust_analyzer.setup({})
+      lspconfig.clangd.setup({})
+    end,
+  },
 }
 
