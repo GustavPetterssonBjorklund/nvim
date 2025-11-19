4195156a1d6b69d32cd9ff31aef8832f7948595c
Gustav Pettersson <gustav.pettersson2@outlook.com>
2025-11-19 11:12:16 +0100

    auto lsp install should work?

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,29 +1,42 @@
 return {
     {
-        "mason-org/mason.nvim",
+        "williamboman/mason.nvim",
         opts = {
             ui = {
                 icons = {
                     package_installed = "✓",
                     package_pending = "➜",
-                    package_uninstalled = "✗"
-                }
-            }
-        }
+                    package_uninstalled = "✗",
+                },
+            },
+        },
     },
+
     {
-        "mason-org/mason-lspconfig.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "neovim/nvim-lspconfig",
+        },
         opts = {
             ensure_installed = {
                 "lua_ls",
                 "pyright",
                 "rust_analyzer",
                 "clangd",
-            }
+            },
+            automatic_installation = true, -- auto-install missing LSPs
         },
-        dependencies = {
-            { "mason-org/mason.nvim", opts = {} },
-            "neovim/nvim-lspconfig",
-        },
+        config = function(_, opts)
+            require("mason-lspconfig").setup(opts)
+
+            -- Auto-setup every installed server
+            require("mason-lspconfig").setup_handlers({
+                function(server)
+                    require("lspconfig")[server].setup({})
+                end,
+            })
+        end,
     },
 }
+
